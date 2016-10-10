e3c6959f352041e78d2406627d3f0f2c03ac2a59
Nicolas Briemant <nicolas.briemant@gmail.com>
2016-10-10 15:50:01 +0200

    import plugin and common imports

--- babel-config.js
+++ babel-config.js
@@ -1,4 +1,12 @@
+// should in wce client config, but comonImports are considered as boilerplates
+// and declaring them is not harmful for performance.
+// So, let's save some time for cool developments :-)
+const commonImports = [
+  { libraryName: "antd" },
+  { libraryName: "lodash", "libraryDirectory": "", camel2DashComponentName: false }
+];
+
 module.exports = {
   "presets": ["react", "es2015", "stage-0"],
-  "plugins": ["transform-decorators-legacy"]
+  "plugins": ["transform-decorators-legacy", ["import", commonImports]]
 };
