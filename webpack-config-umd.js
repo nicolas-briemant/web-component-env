527116b849eeac047cd5ae124ecba707d067e3a8
Nicolas Briemant <nicolas.briemant@gmail.com>
2016-10-10 14:01:20 +0200

    fix missing lib entry point while building app

--- webpack-config-umd.js
+++ webpack-config-umd.js
@@ -4,30 +4,31 @@ const merge = require('webpack-merge');
 
 const common = require('./webpack-config-common');
 
-const buildPath = path.resolve('./dist');
-const srcPath = path.resolve('./src');
-const umdModule = 'lib';
-const umdFilename = umdModule + '.js';
-const umdPath = path.resolve(srcPath, umdModule); // entry point
+module.exports = function(umdModule) {
+  const buildPath = path.resolve('./dist');
+  const srcPath = path.resolve('./src');
+  const umdFilename = umdModule + '.js';
+  const umdPath = path.resolve(srcPath, umdModule); // entry point
 
-module.exports = merge(common, {
-  devtool: 'cheap-module-source-map',
-  entry: umdPath,
-  output: {
-    path: buildPath,
-    filename: umdFilename,
-    libraryTarget: 'umd',
-    umdNamedDefine: true
-  },
-  plugins: [
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.optimize.UglifyJsPlugin({
-      compressor: {
-        pure_getters: true,
-        unsafe: true,
-        unsafe_comps: true,
-        warnings: false
-      }
-    })
-  ]
-});
+  return merge(common, {
+    devtool: 'cheap-module-source-map',
+    entry: umdPath,
+    output: {
+      path: buildPath,
+      filename: umdFilename,
+      libraryTarget: 'umd',
+      umdNamedDefine: true
+    },
+    plugins: [
+      new webpack.optimize.OccurenceOrderPlugin(),
+      new webpack.optimize.UglifyJsPlugin({
+        compressor: {
+          pure_getters: true,
+          unsafe: true,
+          unsafe_comps: true,
+          warnings: false
+        }
+      })
+    ]
+  });
+};
