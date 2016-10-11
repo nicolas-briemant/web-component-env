546a44f524a1e9a493a4c69e6c691c0fe8a6820c
Nicolas Briemant <nicolas.briemant@gmail.com>
2016-10-11 19:10:09 +0200

    define node env for dev

--- webpack-config-dev.js
+++ webpack-config-dev.js
@@ -28,6 +28,11 @@ module.exports = merge(common, {
     port: process.env.PORT
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin()
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': '"development"'
+      }
+    }),
   ]
 });
