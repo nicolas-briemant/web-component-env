99fc6381063b1725e67da1571fb6cf6904af47da
Nicolas Briemant <nicolas.briemant@gmail.com>
2017-05-30 10:48:18 +0200

    add APP_ENV glob for config purpose

--- webpack-config-umd.js
+++ webpack-config-umd.js
@@ -43,7 +43,8 @@ module.exports = function(umdModule, relative) {
       new webpack.optimize.OccurenceOrderPlugin(),
       new webpack.DefinePlugin({
         'process.env': {
-          'NODE_ENV': JSON.stringify(process.env.NODE_ENV) || '"production"'
+          'NODE_ENV': JSON.stringify(process.env.NODE_ENV) || '"production"',
+          'APP_ENV': JSON.stringify(process.env.APP_ENV)
         }
       }),
       new webpack.optimize.UglifyJsPlugin({
